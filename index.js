9eea1d48d4cbfc9755d0968acf3046ed6a9dd80e
Anmol <anmolkumar58575@gmail.com>
2024-02-20 14:37:16 +0530

    exercise

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// exercise 1
+
 let userInput = prompt("Please enter your num : ");
 // let uinput = parseInt(userInput)
 const num =Math.floor(Math.random() * 100);
@@ -17,3 +19,79 @@ while (userInput != num ){
 
 const score = 100 - turn;
 console.log(`you guessed this numer and score is  ${score} in ${turn} TURNS`)
+
+
+// exercise 2
+const ageconf = (age) => {
+    return age > 18 ? true : false;
+}
+// console.log(age);
+let runagain = true;
+
+while (runagain) {
+let userInputt = prompt("Please enter your age : ");
+let age = parseInt(userInputt);
+
+if (ageconf(age)) {
+    alert("Ohh yes, you can drive ");
+} else {
+    alert("Ohh no, you can't drive ");
+}
+runagain = confirm("Do you want to play again?");
+}
+
+
+// exercise 3
+const ageconf = (age) => {
+    return age > 18 ? true : false;
+}
+// console.log(age);
+let runagain = true;
+
+while (runagain) {
+
+let userInputt = prompt("Please enter your age : ");
+let age = parseInt(userInputt);
+ 
+if(age<0){
+    console.error("please enter valid age");
+       break;
+}
+
+if (ageconf(age)) {
+    alert("Ohh yes, you can drive ");
+} else {
+    alert("Ohh no, you can't drive ");
+}
+runagain = confirm("Do you want to play again?");
+}
+
+
+// exercise 4
+const ageconf = (age) => {
+    return age > 18 ? true : false;
+}
+// console.log(age);
+let runagain = true;
+
+while (runagain) {
+
+let userInputt = prompt("Please enter your age : ");
+let age = parseInt(userInputt);
+ 
+if(age<0){
+    console.error("please enter valid age");
+       break;
+}
+if(age = 4){
+    window.href ="https://google.com";
+    break;
+}
+
+if (ageconf(age)) {
+    alert("Ohh yes, you can drive ");
+} else {
+    alert("Ohh no, you can't drive ");
+}
+runagain = confirm("Do you want to play again?");
+}
