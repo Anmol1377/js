e95336f0b821d06422c041d7f2d574bf9d54b811
Anmol <anmolkumar58575@gmail.com>
2024-02-24 19:39:06 +0530

    callback

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 let userInput = prompt("Please enter your num : ");
 // let uinput = parseInt(userInput)
-const num =Math.floor(Math.random() * 100);
+const num = Math.floor(Math.random() * 100);
 console.log(num);
 let turn = 0;
 
-while (userInput != num ){
-    if ( userInput > num){
-    userInput =   prompt("number is greater , please choose low digit: ");
+while (userInput != num) {
+    if (userInput > num) {
+        userInput = prompt("number is greater , please choose low digit: ");
     }
-    if ( userInput < num){
-          userInput = prompt("number is low , please choose high digit: ");
+    if (userInput < num) {
+        userInput = prompt("number is low , please choose high digit: ");
     }
-     
+
     turn++
 }
 
@@ -29,15 +29,15 @@ const ageconf = (age) => {
 let runagain = true;
 
 while (runagain) {
-let userInputt = prompt("Please enter your age : ");
-let age = parseInt(userInputt);
+    let userInputt = prompt("Please enter your age : ");
+    let age = parseInt(userInputt);
 
-if (ageconf(age)) {
-    alert("Ohh yes, you can drive ");
-} else {
-    alert("Ohh no, you can't drive ");
-}
-runagain = confirm("Do you want to play again?");
+    if (ageconf(age)) {
+        alert("Ohh yes, you can drive ");
+    } else {
+        alert("Ohh no, you can't drive ");
+    }
+    runagain = confirm("Do you want to play again?");
 }
 
 
@@ -50,20 +50,20 @@ let runagain = true;
 
 while (runagain) {
 
-let userInputt = prompt("Please enter your age : ");
-let age = parseInt(userInputt);
- 
-if(age<0){
-    console.error("please enter valid age");
-       break;
-}
+    let userInputt = prompt("Please enter your age : ");
+    let age = parseInt(userInputt);
 
-if (ageconf(age)) {
-    alert("Ohh yes, you can drive ");
-} else {
-    alert("Ohh no, you can't drive ");
-}
-runagain = confirm("Do you want to play again?");
+    if (age < 0) {
+        console.error("please enter valid age");
+        break;
+    }
+
+    if (ageconf(age)) {
+        alert("Ohh yes, you can drive ");
+    } else {
+        alert("Ohh no, you can't drive ");
+    }
+    runagain = confirm("Do you want to play again?");
 }
 
 
@@ -76,24 +76,24 @@ let runagain = true;
 
 while (runagain) {
 
-let userInputt = prompt("Please enter your age : ");
-let age = parseInt(userInputt);
- 
-if(age<0){
-    console.error("please enter valid age");
-       break;
-}
-if(age = 4){
-    window.href ="https://google.com";
-    break;
-}
+    let userInputt = prompt("Please enter your age : ");
+    let age = parseInt(userInputt);
 
-if (ageconf(age)) {
-    alert("Ohh yes, you can drive ");
-} else {
-    alert("Ohh no, you can't drive ");
-}
-runagain = confirm("Do you want to play again?");
+    if (age < 0) {
+        console.error("please enter valid age");
+        break;
+    }
+    if (age = 4) {
+        window.href = "https://google.com";
+        break;
+    }
+
+    if (ageconf(age)) {
+        alert("Ohh yes, you can drive ");
+    } else {
+        alert("Ohh no, you can't drive ");
+    }
+    runagain = confirm("Do you want to play again?");
 }
 
 
@@ -103,18 +103,46 @@ alert("please enter rock,paper,scier");
 let user = prompt("Please enter your choice: ");
 
 let roboIndex = Math.floor(Math.random() * 3);
-let roboChoicearr = ["rock","paper","scier"];
+let roboChoicearr = ["rock", "paper", "scier"];
 let roboChoice = roboChoicearr[roboIndex]
 console.log(roboChoice);
 var userconfrm = false;
-while (userconfrm == false){
-if(user == roboChoice){
-    console.log("its tie");
+while (userconfrm == false) {
+    if (user == roboChoice) {
+        console.log("its tie");
+    }
+    else if (user == "rock" && roboChoice == "scier" || user == "paper" && roboChoice == "rock" || user == "scier" && roboChoice == "paper") {
+        console.log("you win");
+    } else {
+        console.log("computer win");
+    }
+    userconfrm = confirm("do you want to play again");
 }
-else if (user == "rock" && roboChoice == "scier" || user == "paper" && roboChoice == "rock" || user == "scier" && roboChoice == "paper"){
-    console.log("you win");
-}else {
-    console.log("computer win");
+
+
+
+// callback 
+
+// For those who need more clearance can look at the below callback function
+
+function add(a, b) {
+    return a + b;
 }
- userconfrm = confirm("do you want to play again");
+
+function divide(a, b) {
+    return a / b;
 }
+
+
+function calculate(x, y, operation) {
+    return operation(x, y);
+}
+
+
+// In the first console.log, you are calling calculate with the numbers 10 and 5 and passing the add function as the operation. Therefore, it calculates add(10, 5) which results in 15.
+
+// In the second console.log, you are calling calculate again with the numbers 10 and 5, but this time passing the divide function as the operation. Therefore, it calculates divide(10, 5) which results in 2.
+
+console.log(calculate(10, 5, add));
+
+console.log(calculate(10, 5, divide));
